7e465298d7a3ac2ae5b81d8fba2d162d62af229e
MijaDs <106399497+MijaDs@users.noreply.github.com>
2025-02-25 23:06:23 -0600

    Mija
    Agrego usuario

---
+++ G4_SC701_CasoPractico1.Rutas/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace G4_SC701_CasoPractico1.Rutas.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string NombreCompleto { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Telefono { get; set; }
+        public string Contraseña { get; set; }
+        
+        public Rol Rol { get; set; }
+    }
+}
