ab4499b115d237cd766f2a3c8cf7a31c6bdc7d4f
MijaDs <106399497+MijaDs@users.noreply.github.com>
2025-02-25 22:14:27 -0600

    Mija
    Vistas usuario y rol, se agragan las primeras migraciones a la BD

--- G4_SC701_CasoPractico1.Rutas/Models/CP1Context.cs
+++ G4_SC701_CasoPractico1.Rutas/Models/CP1Context.cs
@@ -6,7 +6,39 @@ namespace G4_SC701_CasoPractico1.Rutas.Models
     {
         public CP1Context(DbContextOptions<CP1Context> options) : base(options){ }
 
-        //DBsets
+        public object Usuario { get; internal set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(Usuario =>
+            {
+                Usuario.HasKey(u => u.Id);
+                Usuario.Property(u => u.NombreUsuario).HasMaxLength(50).IsRequired().HasColumnName("Usuario");
+                Usuario.Property(u => u.NombreCompleto).HasMaxLength(100).IsRequired().IsUnicode(true);
+                Usuario.Property(u => u.CorreoElectronico).HasMaxLength(100).IsRequired();
+            });
+            modelBuilder.Entity<Rol>(Rol =>
+            {
+                Rol.HasKey(r => r.Id);
+                Rol.Property(r => r.Nombre).HasMaxLength(50).IsRequired();
+
+            });
+
+            modelBuilder.Entity<Usuario>().HasOne<Rol>(u => u.Rol)
+                                          .WithMany(r => r.Usuarios)
+                                          .HasForeignKey(u => u.Id)
+                                          .HasConstraintName("FK_Usuario_Rol")
+                                          .OnDelete(DeleteBehavior.Cascade);
+        }
         
     }
 }
+
+
+    
---
+++ G4_SC701_CasoPractico1.Rutas/Models/Rol.cs
@@ -0,0 +1,10 @@
+﻿namespace G4_SC701_CasoPractico1.Rutas.Models
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public IEnumerable<Usuario>? Usuarios { get; set; }
+    }
+}
